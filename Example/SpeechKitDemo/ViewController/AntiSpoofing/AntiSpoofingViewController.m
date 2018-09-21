c96b8e518b72ac844b0b9b76022a086662f72e3d
alexandrasoloshcheva <soloshcheva@speechpro.com>
2018-09-21 16:48:57 +0300

    AntiSpoofing

---
+++ Example/SpeechKitDemo/ViewController/AntiSpoofing/AntiSpoofingViewController.m
@@ -0,0 +1,77 @@
+//
+//  AntiSpoofingViewController.m
+//  STCSpeechKitDemo
+//
+//  Created by Soloshcheva Aleksandra on 18.09.2018.
+//  Copyright © 2018 Speech Tehnology Center. All rights reserved.
+//
+
+#import "AntiSpoofingViewController.h"
+
+#import <STCSpeechKit.h>
+
+@interface AntiSpoofingViewController ()
+
+@end
+
+@implementation AntiSpoofingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
+
+/*
+ @interface DiarizationViewController ()
+ 
+ @property (nonatomic) id<STCDiarizating> diarizator;
+ 
+ @property (nonatomic,weak) IBOutlet UITextView *textView;
+ 
+ @end
+ 
+ @interface DiarizationViewController (Private)
+ 
+ -(void)showResult:(NSString *)result;
+ 
+ @end
+ 
+ @implementation DiarizationViewController
+ 
+ - (void)viewDidLoad {
+ [super viewDidLoad];
+ self.diarizator = STCSpeechKit.sharedInstance.diarizator;
+ self.textView.text = @"";
+ }
+ 
+ -(IBAction)onPlay:(id)sender {
+ if (self.isStopMode) {
+ [self.diarizator stop];
+ [self showActivityIndicator];
+ } else {
+ [self configureButtonAsStop];
+ self.textView.text = @"";
+ 
+ [self.diarizator startWithCompletionHandler:^(NSError *error, NSDictionary *result) {
+ [self showResult:[NSString stringWithFormat:@"%@",result]];
+ [self configureButtonAsPlay];
+ [self hideActivityIndicator];
+ }];
+ }
+ }
+ 
+ @end
+ 
+ @implementation DiarizationViewController (Private)
+ 
+ -(void)showResult:(NSString *)result {
+ dispatch_async(dispatch_get_main_queue(), ^{
+ self.textView.text = result;
+ });
+ }
+ 
+ @end
+ */
